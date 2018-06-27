fd26679d807922e92fd5cad6a29b3a77cd6eca80
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-06-27 18:14:14 -0500

    added components printed them to the page and added seed data

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 import Animals from '../components/Animals/Animals';
+import Form from '../components/Form/Form';
 
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Animals />
+      <div className="wrapper">
+        <div className="col-md-6 text-center">
+          <Animals />
+        </div>
+        <div className="col-md-6 text-center">
+          <Form />
+        </div>
+      </div>
     );
   }
 }
