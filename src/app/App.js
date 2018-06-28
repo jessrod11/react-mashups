5f1d37aba7b1960ebb80920a18b8d4a9330ce731
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-06-27 19:32:56 -0500

    printing names to the dom

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 
 import connection from '../FirebaseRequests/connection';
+import animalRequest from '../FirebaseRequests/animals';
 import Animals from '../components/Animals/Animals';
 import Form from '../components/Form/Form';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    animals: [],
+  }
+
   componentDidMount () {
     connection();
+    animalRequest.getRequest()
+      .then((animals) => {
+        this.setState({animals});
+      })
+      .catch((error) => {
+        console.error('error in GET animal Request', error);
+      });
   }
 
   render () {
     return (
       <div className="wrapper">
         <div className="col-md-6 text-center">
-          <Animals />
+          <Animals animals={this.state.animals}/>
         </div>
         <div className="col-md-6 text-center">
           <Form />
--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -4,9 +4,21 @@ import './Animals.css';
 
 class Animals extends React.Component {
   render () {
+    const {animals} = this.props;
+    const animalCardComponents = animals.map((animal) => {
+      return (
+        <div>
+          <h2 key={animal.id}>{animal.name}</h2>
+        </div>
+      );
+    });
+
     return (
       <div className="Animals">
         <h2>Animals</h2>
+        <div>
+          <h1>{animalCardComponents}</h1>
+        </div>
       </div>
     );
   };
