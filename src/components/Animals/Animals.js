a91241fca6e6bf19a424f948cfd951b088eae09d
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-02 18:00:01 -0500

    printing animal info to the page

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -7,8 +7,12 @@ class Animals extends React.Component {
     const {animals} = this.props;
     const animalCardComponents = animals.map((animal) => {
       return (
-        <div>
-          <h2 key={animal.id}>{animal.name}</h2>
+        <div className= "row">
+          <div className="col-md-4">
+            <h2 key={animal.id}>{animal.name}</h2>
+            <img class="animal-img" src={animal.imgUrl} alt="animal pic"/>
+            <h3>{animal.description}</h3>
+          </div>
         </div>
       );
     });
