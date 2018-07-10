dfd702c81fcd1993e45a8fad2d42531b6172afba
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-10 11:20:56 -0500

    added form to page

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,7 +5,29 @@ class Form extends React.Component {
   render () {
     return (
       <div>
-        <h2 className="Form">Form</h2>
+        <h2>Add an Animal below:</h2>
+        <form className="Form">
+          <div className="form-group">
+            <div className="col-m-10">
+              <input type="text" className="form-control text-center" placeholder="Name..."/>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-m-10">
+              <input type="text" className="form-control text-center" placeholder="Image URL..."/>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-m-10">
+              <textarea cols="30" rows="10" className="text-styles text-center" placeholder="Add Description..."></textarea>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-md-3 col-md-offset-3">
+              <button className="btn btn-info btn-lg button-styles">Add Animal</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
